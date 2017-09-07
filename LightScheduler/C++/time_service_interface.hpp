3db3f9e5f3fa19fa3809e0d14089e808f5baf05d
Michel Estermann <estermann.michel@gmail.com>
2017-09-07 20:04:36 +0200

    Created Light scheduler custom start point.

---
+++ LightScheduler/C++/time_service_interface.hpp
@@ -0,0 +1,27 @@
+#ifndef TIME_SERVICE_INTERFACE_INCLUDED
+#define TIME_SERVICE_INTERFACE_INCLUDED
+
+#include <chrono>
+#include <functional>
+
+class ITimeService
+{
+public:
+    struct time_t {
+        int second;
+        int minute;
+        int hour;
+        int day_of_month;
+        int month;
+        int year;
+        int day_of_week;
+    };s
+
+    using wackup_callback = std::function<void()>;
+
+    virtual ~ITimeService() {};
+    virtual time_t get_time() const = 0;
+    virtual void set_periodic_alarm(std::chrono::minutes period, wackup_callback callback) = 0;
+};
+
+#endif
