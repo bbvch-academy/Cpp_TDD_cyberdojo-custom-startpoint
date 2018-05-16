9cb906501ee5c04b255d2872417bc25fa17dd6c0
Michel Estermann <estermann.michel@gmail.com>
2018-05-16 21:07:52 +0200

    Changed ITimeService time type to std::tm.

--- LightScheduler/C++/time_service_interface.hpp
+++ LightScheduler/C++/time_service_interface.hpp
@@ -4,25 +4,17 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <ctime>
 
 class ITimeService
 {
 public:
-    struct time_t {
-        int second;
-        int minute;
-        int hour;
-        int day_of_month;
-        int month;
-        int year;
-        int day_of_week;
-    };
-
     using wakeup_callback = std::function<void()>;
     using alarm_handle = std::shared_ptr<void>;
+    using date_time = std::tm;
 
     virtual ~ITimeService() = default;
-    virtual time_t current_time() const = 0;
+    virtual date_time current_time() const = 0;
     virtual alarm_handle start_periodic_alarm(std::chrono::minutes period,
                                               wakeup_callback callback) = 0;
     virtual void stop_periodic_alarm() = 0;
